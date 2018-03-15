088277ed3764070b54f93cb741b0c4374b207fb1
Matej Sajgal <msajgal@switch.tv>
2018-03-15 19:19:15 +1100

    Add sign up page styles.

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import '../styles/sign-up.css'
+import FaFacebook from 'react-icons/lib/fa/facebook';
 
 const SignUpPage = () => (
-  <div>
+  <div className="signup-box">
     <Helmet
       title="DailyUI 01 - Sign up"
       meta={[
@@ -11,11 +12,21 @@ const SignUpPage = () => (
         {name: 'keywords', content: 'dailyui, signut, login'},
       ]}
     />
-    <h1>Sign up</h1>
+    <h1 className="title">Daily UI Inc.</h1>
     <form action="#">
-      <input type="email" placeholder="email" />
-      <input type="submit" value="Register"/>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" placeholder="username"/>
+
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" placeholder="your@email.com"/>
+
+      <input type="submit" value="Sign up"/>
     </form>
+    <div>
+      <button className="facebook-sign-up"><FaFacebook/> Sign up with Facebook</button>
+    </div>
+
+    <div className="already-registered">Already have an account? <a href="#">Log in here.</a></div>
   </div>
 )
 
